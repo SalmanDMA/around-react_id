3bc680499f31791823a7628b60fba05d9774de63
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-06-21 13:37:36 +0700

    Masih memperbaiki masalah lift up state component main ketiga

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { useEffect, useRef, useState } from 'react';
+import { CurrentUserContext } from './contexts/CurrentUserContext';
+import api from './utils/api';
 import Header from './components/Header';
 import Main from './components/Main';
 import EditProfilePopup from './components/EditProfilePopup';
 import AddPlacepopup from './components/AddPlacePopup';
 import EditAvatarPopup from './components/EditAvatarPopup';
 import Footer from './components/Footer';
-import { useEffect, useRef, useState } from 'react';
-import { CurrentUserContext } from './contexts/CurrentUserContext';
-import api from './utils/api';
 
 function App() {
  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
