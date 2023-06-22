25f3525eeee177cf3071284bdd244c9a243052bc
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-06-22 17:32:26 +0700

    fixing again lift the state up main component part 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,14 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 import Main from './Main';
 
 // Membuat konteks
 const CurrentUserContext = createContext();
 
 function App() {
- // Mendefinisikan state di komponen App
- const [currentUser, setCurrentUser] = useState(null);
-
- // Membuat variabel untuk provider konteks
- const currentUserProvider = {
-  currentUser,
-  setCurrentUser,
- };
-
  return (
   <div>
    {/* Membungkus bagian yang relevan dari struktur komponen dengan provider konteks */}
-   <CurrentUserContext.Provider value={currentUserProvider}>
+   <CurrentUserContext.Provider>
     <Main />
    </CurrentUserContext.Provider>
   </div>
